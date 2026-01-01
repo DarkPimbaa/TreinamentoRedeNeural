d8387ba48457e79154234577e460f06c5c58c44c
Carlos <141370599+DarkPimbaa@users.noreply.github.com>
2025-12-31 23:20:41 -0300

    Change training time from 1.0 to 60.0 minutes

--- src/main.cu
+++ src/main.cu
@@ -2,7 +2,7 @@
 #define NUM_CAMADAS 4
 #define NUM_NEURONIOS 608 // multiplo de 16
 #define NUM_SAIDAS 2
-#define TEMPO_TREINAMENTO 1.0 // tempo total de treinamento em minutos
+#define TEMPO_TREINAMENTO 60.0 // tempo total de treinamento em minutos
 #define NUM_CANDLES_BATCH 900'000 // numero de candles que cada individuo vai "apostar" antes da verificação
 #define TAMANHO_BTCCSV 954620
 
